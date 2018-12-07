4906818718b07aec621cb2ebfbde5b89f566f76c
Sowintuu <sowintuu@aol.com>
2018-12-07 15:22:40 +0100

    Moved Changelog to file

--- src/voiceAtis.py
+++ src/voiceAtis.py
@@ -16,34 +16,6 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# 
-#==============================================================================
-# CHANGELOG
-#
-# version 0.0.3 - 07.12.2018
-# - Now using metar if no ATIS available
-# - pyuipc tested and running
-# - Changed RADIO_RANGE to a (realistic) value of 180 nm
-# - Implemented logging
-#
-# version 0.0.2 - 05.12.2018
-# - Implemented wind gusts and variable wind
-# - Port to python2 (due to pyuipc)
-# - Added pyuipc (untested)
-# - Added logic to get airport
-# 
-# version 0.0.1 - 03.12.2018
-# - First version for testing purposes
-# - Some Atis feartures missing
-# - No pyuipc
-# - Voice not tested
-# 
-#==============================================================================
-# ROADMAP
-# 
-# - random start
-# - get ivac2 atis running
-# 
 #==============================================================================
 
 import os
